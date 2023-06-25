48680a4e6299742f4c7ea5d2a60839a577a94c4c
arkadii <sheglovaoe@gmail.com>
2023-06-25 08:32:07 +0300

    optimized fs scan

--- backend/z_env.js
+++ backend/z_env.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
-import {get_db, get_zon_rel_path} from './utils.js';
+import {get_db} from './utils.js';
+import {dur2str} from '../lib/utils.js';
 
 const is_zon_root = async z_dir => {
     let file_or_folder = path.join(z_dir, 'pkg');
@@ -12,17 +13,104 @@ const is_zon_root = async z_dir => {
     if (!fs.existsSync(file_or_folder))
         return false;
 
-    try {
-        let txt = await fs.promises.readFile(file_or_folder, 'utf8');
-        txt = txt.trim();
-        if (txt != 'zon')
-            return false;
-    } catch (e) {
-        console.error('Error during zon dir check:', e);
-        return false;
-    }
-    return true;
+    let txt = await fs.promises.readFile(file_or_folder, 'utf8')
+        .catch(e => console.error('Error during zon dir check:', e) || '')
+        .then(x => x.trim());
+
+    return txt == 'zon';
 };
+
+class FileMap {
+    /**
+     * Navigating field
+     * @type {Map<string, FileMap>}
+     */
+    static all_files = new Map();
+
+    /**
+     * @param abs_path {string}
+     * @param zon_rel {string}
+     * @param stat {Stats}
+     */
+    constructor(abs_path, zon_rel, stat) {
+        this.abs_path = abs_path;
+        this.relative_path = zon_rel;
+        this.types = new Set();
+        if (stat?.isDirectory()) {
+            /**
+             * @type {FileMap[]}
+             */
+            this.children = [];
+            this.types.add('folder');
+        }
+        FileMap.all_files.set(abs_path, this);
+        this.parent?.children.push(this)
+    }
+
+    get parent() {
+        let parent = FileMap.all_files.get(path.dirname(this.abs_path));
+        if (parent?.children)
+            return parent;
+    }
+
+    get flat_children() {
+        let children = Array.from(FileMap.all_files.keys()).filter(x => x.startsWith(this.abs_path));
+        return children.map(x => FileMap.all_files.get(x));
+    }
+
+    add_type(type) {
+        if (!'selenium mocha folder ignored file'.split(' ').includes(type))
+            console.warn('Unsupported file type:', type);
+        else {
+            this.types.add(type);
+            this.parent?.types.add(type);
+        }
+    }
+
+    /**
+     * @param runs {TestRun[]}
+     * @param ignored {IgnoredTest[]}
+     */
+    init_from_test_db(runs, ignored) {
+        runs = runs.filter(x => x.file == this.relative_path);
+        ignored = ignored.find(x => x.file == this.relative_path);
+        if (!runs.length)
+            return false;
+
+        if (ignored)
+            this.add_type('ignored')
+
+        let r = runs.find(x => x.type)
+        if (r)
+            this.add_type(r.type);
+        runs = runs.filter(x => x.result == 'fail' || x.result == 'success');
+        if (runs.length) {
+            this.success = runs.filter(x => x.result == 'success').length;
+            this.fail = runs.filter(x => x.result == 'fail').length;
+            this.avg = runs.map(x => x.end - x.start).reduce((s, c) => s + c, 0) / runs.length;
+            let last_run = runs.sort((a, b) => a.start - b.start)[0];
+            this.last_run_failed = last_run.result == 'fail';
+            this.last_run_date = last_run.start;
+        }
+        return true;
+    }
+
+    /**
+     * @return {File}
+     */
+    toJSON() {
+        const res = {
+            filename: path.basename(this.abs_path),
+            fullpath: this.abs_path
+        };
+        'success fail avg last_run_failed last_run_date'.split(' ')
+            .filter(x=>this.hasOwnProperty(x)).forEach(x=>res[x] = this[x]);
+        res.types = Array.from(this.types).filter(x=>x != 'file');
+        if (this.children)
+            res.children = this.children.map(x => x.toJSON());
+        return res;
+    }
+}
 
 export class ZonDir {
     constructor(abs_path) {
@@ -31,50 +119,41 @@ export class ZonDir {
         this.watch_dir = path.join(abs_path, 'pkg');
     }
 
-    get_zon_rel_path(abs_path) {
+    _get_zon_rel_path(abs_path) {
         return path.relative(this.zon_root, abs_path);
     }
-    async get_file_type (filename){
-        let q_id = {last_result: 'init', file: await this.get_zon_rel_path(filename)};
-        if (this.test_runs_db) {
-            let file = await this.test_runs_db.findOneAsync(q_id);
-            if (file)
-                return file.type;
-        }
-        if (filename.endsWith('.js')) {
-            let txt = await fs.promises.readFile(filename, 'utf8');
-            if (/^describe\(/g.test(txt) || txt.includes(' describe(')) {
-                let type = txt.includes('selenium.') ? 'selenium' : 'mocha';
-                if (this.test_runs_db)
-                    await this.test_runs_db.updateAsync(q_id, {...q_id, type}, {upsert: true});
-                return type;
+
+    /**
+     * @param test_file {FileMap}
+     * @param runs {TestRun[]}
+     * @param ignored {IgnoredTest[]}
+     * @return {Promise<void>}
+     */
+    async set_file_type(test_file, runs, ignored) {
+        if (!test_file.init_from_test_db(runs, ignored)) {
+            if (test_file.abs_path.endsWith('.js')) {
+                let txt = await fs.promises.readFile(test_file.abs_path, 'utf8');
+                let type = 'file';
+                if (/^describe\(/g.test(txt) || txt.includes(' describe(')) {
+                    type = txt.includes('selenium.') ? 'selenium' : 'mocha';
+                    test_file.add_type(type);
+                }
+                return {
+                    //  \/\/ constraint \/\/
+                    file: test_file.relative_path,
+                    start: new Date(),
+                    // ^^^^ constraint ^^^^^^
+                    result: 'init',
+                    type,
+                };
             }
-            else
-            {
-                if (this.test_runs_db)
-                    await this.test_runs_db.updateAsync(q_id, {...q_id, type: 'file'}, {upsert: true});
-            }
         }
-        return 'file';
-    };
-    
-    async print_dbg_info() {
-        let total = await this.files.countAsync({});
-        let mocha = await this.files.countAsync({type: 'mocha'});
-        let selenium = await this.files.countAsync({type: 'selenium'});
-        let folders = await this.files.countAsync({type: 'folder'});
-        console.debug(`[${this.dirname}] Scanned ${total} paths, [${folders}] folders, founded [${mocha}] mocha tests, `
-        +`[${selenium}] selenium tests`);
     }
 
     async _init() {
-        console.debug('Read', this.zon_root, 'folder');
+        console.debug('Starting scanning', this.zon_root, 'folder');
+        let now = new Date();
 
-        /** @type {Nedb} */
-        this.files = await get_db(undefined, {
-            indexes: [
-                {fieldName: 'filename'},]
-        }); // in memory db
         /** @type {Nedb<TestRun>}*/
         this.test_runs_db = await get_db('/test_runs.jsonl', {
             indexes: [{unique: true, fieldName: 'file start'.split(' ')}],
@@ -85,117 +164,53 @@ export class ZonDir {
             indexes: [{unique: true, fieldName: 'file'}],
             singleton: true,
         });
-        await this.files.removeAsync({}, {multi: true});
-        let i = setInterval(() => this.print_dbg_info(), 1000);
-        await this.read_file(this.watch_dir);
-        // this.watcher = fs.watch(this.watch_dir, {encoding: 'utf8', recursive: true, persistent: true},
-        //     this.handle_file_change);
-        clearInterval(this.recheck_meta_i);
-        this.recheck_meta_i = setInterval(this.recheck_meta, 1000 * 5);
-        clearInterval(i);
-        console.debug('FILE SCAN DONE');
-        await this.print_dbg_info();
-    }
 
-    async read_file(abs_path) {
-        if (!fs.existsSync(abs_path))
-            return;
-
-        let stat = await fs.promises.stat(abs_path).catch(e => {
-            console.warn('Error during file read:', e);
-        });
-        const q_id = {filename: abs_path}, rel_q_id = {file: await get_zon_rel_path(abs_path)};
-        const parent_q_id = {filename: path.dirname(abs_path)};
-
-        if (stat?.isDirectory())
-        {
-            await this.files.updateAsync(q_id, {...q_id, type: 'folder', children: []}, {upsert: true});
-            await this.files.updateAsync(parent_q_id, {$push: {children: abs_path}, $set: parent_q_id});
-            let children = await fs.promises.readdir(abs_path)
-            children = children.map(x => path.join(abs_path, x));
-            await Promise.all(children.map(x => this.read_file(x)));
-        } else if (stat?.isFile())
-        {
-            const type = await this.get_file_type(abs_path, this.test_runs_db);
-            const runs = await this.test_runs_db.findAsync(rel_q_id);
-            const ignore_info = await this.ignored_tests_db.findOneAsync(rel_q_id);
-            await this.files.updateAsync(q_id, {
-                $set: {
-                    runs: runs || [],
-                    ...ignore_info || {},
-                    ...q_id, type,
-                },
-                $unset: {children: ''},
-            }, {upsert: true});
-            const upd = {$set: {}, $push: {children: abs_path}};
-            if (type == 'mocha')
-                upd.$set.has_mocha = true;
-            if (type == 'selenium')
-                upd.$set.has_selenium = true;
-            if (ignore_info?.ignore_reason)
-                upd.$set.has_ignored = true;
-
-            await this.files.updateAsync(parent_q_id, upd);
-        } else {
-            console.warn(`Unknown file: ${abs_path}`);
+        let map = await this._fs_init(this.watch_dir);
+        for (let [abs_path, stat] of map.entries()) {
+            new FileMap(abs_path, this._get_zon_rel_path(abs_path), stat);
         }
+        this.root = FileMap.all_files.get(this.watch_dir);
+        let [runs, ignored] = await Promise.all([
+            this.test_runs_db.findAsync({}),
+            this.ignored_tests_db.findAsync({}),
+        ]);
+        let pending_updates = await Promise.all(this.root.flat_children.filter(x => !x.types.size)
+            .map(x => this.set_file_type(x, runs, ignored)));
+        pending_updates = pending_updates.filter(Boolean);
+        if (pending_updates.length)
+            await this.test_runs_db.insertAsync(pending_updates);
+
+        console.debug('Scanning finished', this.zon_root, ', took', dur2str(new Date() - now, undefined,
+            1));
     }
 
-    async handle_file_change(e, filename) {
-        // todo
-    }
-
-    async recheck_meta() {
-
-    }
-
-    async toJSON() {
-        let flat_tree_map = await this.files.findAsync({});
-        console.debug('Total files:', flat_tree_map.length);
-        flat_tree_map = new Map(flat_tree_map.map(x => [x.filename, x]));
-        /**
-         * @param node {{
-         *     type: 'selenium' | 'mocha' | 'folder' | 'file',
-         *     filename: string,
-         *     has_mocha: boolean | undefined,
-         *     has_selenium: boolean | undefined,
-         *     has_ignored: boolean | undefined,
-         *     runs: TestRun[],
-         * }}
-         * @return {File}
-         */
-        const _to_json = node => {
-            const types = [];
-            if ('selenium mocha folder'.split(' ').includes(node.type))
-                types.push(node.type);
-            'selenium mocha ignored'.split(' ').filter(x => node['has_' + x])
-                .forEach(x => types.push(x));
-            /*** @type {File}*/
-            let test_info = {};
-            if (node.runs?.length)
-            {
-                let [success, fail] = 'success fail'.split(' ')
-                    .map(x=>node.runs.filter(r=>r.result == x));
-                test_info.success = success.length;
-                test_info.fail = fail.length;
-                test_info.avg = [...success, ...fail].map(x=>x.end-x.start)
-                    .reduce((sum, x)=>sum+x, 0) / (success.length + fail.length);
+    /**
+     * @param abs_path {string}
+     * @param map {Map<string, Stats>}
+     * @return {Promise<Map<string, Stats>>}
+     * @private
+     */
+    async _fs_init(abs_path, map) {
+        map = map || new Map();
+        if (fs.existsSync(abs_path)) {
+            let stat = await fs.promises.stat(abs_path)
+                .catch(e => console.error('Error during file stat read:', e));
+            if (stat?.isFile() || stat?.isDirectory())
+                map.set(abs_path, stat);
+            if (stat?.isDirectory()) {
+                await fs.promises.readdir(abs_path)
+                    .then(arr => arr.map(x=> path.join(abs_path, x)))
+                    .catch(x => console.error('Error during directory read:', x))
+                    .then(arr => Promise.all(arr.map(x=>this._fs_init(x, map))))
+                    .catch(e=>console.error('Cannot read zon directory:', e));
             }
-            if (node.children?.length)
-            {
-                test_info.children = node.children.map(x=>flat_tree_map.get(x))
-                    .filter(Boolean).map(x=>_to_json(x));
-            }
-            return {
-                filename: path.basename(node.filename),
-                fullpath: node.filename,
-                types,
-                ...test_info
-            };
-        };
-        let root = _to_json(flat_tree_map.get(this.watch_dir));
+        }
+        return map;
+    }
+
+    toJSON() {
         return {
-            tree: [root],
+            tree: [this.root.toJSON()]
         };
     }
 }
@@ -205,22 +220,18 @@ export class ZonDir {
  * @return {AsyncGenerator<ZonDir, void, *>}
  */
 export async function get_zon_folders() {
-    let dir = os.homedir(), children;
-    try {
-        children = await fs.promises.readdir(dir);
-        children = children.map(x => path.join(dir, x));
-    } catch (e) {
-        console.error('CRIT error during dir reading:', e);
-        throw e;
-    }
-    let result = [];
-    for (let folder of children) {
-        if (await is_zon_root(folder)) {
-            let res = new ZonDir(folder);
-            result.push(res);
-        }
-    }
-    result = result.slice(0, 1);
-    await Promise.all(result.map(x => x._init()));
-    return result;
+    const dir = os.homedir();
+    let children = await fs.promises.readdir(dir)
+        .then(arr => arr.map(s => path.join(dir, s)))
+        .catch(e => {
+            console.error('Cannot read directory:', e);
+            throw e;
+        });
+    children = await Promise.all(children.map(x=>is_zon_root(x).then(y=>y ? x : undefined)));
+    children = children.filter(Boolean).map(x=>new ZonDir(x));
+    let root = children.find(x=>path.basename(x.zon_root) == '.zon');
+    await root._init(); // root folder must init at first
+    let json = root.toJSON();
+    await Promise.all(children.filter(x=>x!=root).map(x => x._init()));
+    return children;
 }
